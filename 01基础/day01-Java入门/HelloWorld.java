e7e135efba0db3b7c6a750232ae48baa1d15d9ab
AsuraTu <tugecn@gmail.com>
2023-04-06 14:29:19 +0800

    init

---
+++ 01基础/day01-Java入门/HelloWorld.java
@@ -0,0 +1,31 @@
+
+// class 类, HelloWorld 类名
+// public 公共的, 修饰符, 用于修饰类, 方法, 变量
+// static 静态的, 修饰符, 用于修饰方法, 变量
+// void 空, 用于修饰方法, 表示方法没有返回值
+public class HelloWorld {
+    // main 方法是程序的入口
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+// javac HelloWorld.java 编译 (javac是JDK中的一个工具，用于将java源文件编译成class文件)
+// java HelloWorld 运行 (java是JDK中的一个工具，用于运行class文件)
+
+// JDK 是 Java Development Kit 的缩写，是 Java 开发工具包的意思。
+// JVM 是 Java Virtual Machine 的缩写，是 Java 虚拟机的意思。
+// JRE 是 Java Runtime Environment 的缩写，是 Java 运行时环境的意思。
+
+// JDK = JRE + 开发工具
+// JRE = JVM + 核心类库
+
+// JavaSE Java Standard Edition  Java标准版 用于桌面应用
+// JavaME Java Micro Edition  Java微型版 用于嵌入式设备, 手机
+// JavaEE Java Enterprise Edition  Java企业版 用于大型企业应用
+
+// q: 什么是字面量
+// a: 字面量就是直接写出来的值, 比如 100, "Hello World", 3.14
+
+// q: 字符串类型和字符类型的区别
+// a: 1. 字符串类型是 String, 字符类型是 char, 2. 字符串类型可以存储多个字符, 字符类型只能存储一个字符, 3. 字符串类型的值必须用双引号括起来, 字符类型的值必须用单引号括起来
---
+++ 01基础/day02-变量/VariableDemo1.java
@@ -0,0 +1,8 @@
+public class VariableDemo1 {
+    public static void main(String[] args) {
+        // 定义变量
+        // 数据类型 变量名 = 初始值;
+        int i = 10;
+        System.out.println(i);
+    }
+}
