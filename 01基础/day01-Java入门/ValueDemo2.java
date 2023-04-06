eeee812733ada00be56b5a26e74fb1352f0c4208
AsuraTu <tugecn@gmail.com>
2023-04-06 18:34:02 +0800

    update

--- 01基础/day01-Java入门/ValueDemo2.java
+++ 01基础/day01-Java入门/ValueDemo2.java
@@ -1,9 +1,8 @@
 public class ValueDemo2 {
-      public static void main(String[] args) {
-          // \t 制表符, 在打印的时候, 把前面的字符串长度补齐到8的倍数, 最少补一个空格, 最多补7个空格
-          System.out.println("name"+"\t"+"Tom");
-          System.out.println("age"+"\t"+"23");
-          // \n 换行符, 在打印的时候, 把前面的字符串打印完毕, 然后换行
-      }
-
+    public static void main(String[] args) {
+        // \t 制表符, 在打印的时候, 把前面的字符串长度补齐到8的倍数, 最少补一个空格, 最多补7个空格
+        System.out.println("name" + "\t" + "Tom");
+        System.out.println("age" + "\t" + "23");
+        // \n 换行符, 在打印的时候, 把前面的字符串打印完毕, 然后换行
+    }
 }
