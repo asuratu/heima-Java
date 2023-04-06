d267b06c4b5939cdf05416a7fcd53d9198ceaf40
AsuraTu <tugecn@gmail.com>
2023-04-06 18:42:09 +0800

    update

--- 01基础/day02-变量/VariableDemo1.java
+++ 01基础/day02-变量/VariableDemo1.java
@@ -4,5 +4,9 @@ public class VariableDemo1 {
         // 数据类型 变量名 = 初始值;
         int i = 10;
         System.out.println(i);
+
+        // 变量的使用
+        int j = 30;
+        System.out.println(j + i);
     }
 }
