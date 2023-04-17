bcbc2205bb4fb552afda30cb7d9e7ff9332cf779
AsuraTu <tugecn@gmail.com>
2023-04-17 16:46:49 +0800

    update

---
+++ mystatic/src/com/heima/a01staticdemo1/Student.java
@@ -0,0 +1,63 @@
+package com.heima.a01staticdemo1;
+
+/**
+ * @ClassName Student
+ * @Description com.heima.a01staticdemo1.Student
+ * @Author AsuraTu
+ * @Date 2023/4/17 16:28
+ * @Version 1.0.0
+ */
+public class Student {
+    // static 修饰的成员变量, 静态变量, 所有的对象共享一份数据
+    // 一般用来存储对象的共性数据
+    // 还可以使用 类名.静态变量 的方式访问 Student.teacherName
+    public static String teacherName;
+    // 姓名 年龄 性别
+    private String name;
+    private String age;
+    private String gender;
+
+    public Student() {
+    }
+
+    public Student(String name, String age, String gender) {
+        this.name = name;
+        this.age = age;
+        this.gender = gender;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void study() {
+        System.out.println(name + " is studying");
+    }
+
+    public void show() {
+        // 打印学生信息
+        // \t 制表符, 在打印的时候, 把前面的字符串长度补齐到8的倍数, 最少补一个空格, 最多补8个空格
+        System.out.println("姓名\t年龄\t性别\t老师\t");
+        System.out.println(name + "\t" + age + "\t" + gender + "\t" + teacherName + "\t");
+    }
+}
---
+++ mystatic/src/com/heima/a01staticdemo1/StudentTest.java
@@ -0,0 +1,26 @@
+package com.heima.a01staticdemo1;
+
+/**
+ * @ClassName StudentTest
+ * @Description com.heima.a01staticdemo1.StudentTest
+ * @Author AsuraTu
+ * @Date 2023/4/17 16:38
+ * @Version 1.0.0
+ */
+public class StudentTest {
+    public static void main(String[] args) {
+// 创建学生对象
+        Student s1 = new Student("张三", "23", "男");
+        // 给学生对象的老师属性赋值
+        s1.teacherName = "张老师";
+        // 调用方法
+        s1.study();
+        s1.show();
+        
+        System.out.println("---------------");
+
+        Student s2 = new Student("李四", "24", "女");
+        s2.study();
+        s2.show();
+    }
+}
