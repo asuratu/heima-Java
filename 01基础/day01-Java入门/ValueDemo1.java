3578e4012f9d15a05dcfc17c724fca5f816ae0a6
AsuraTu <tugecn@gmail.com>
2024-09-03 15:42:18 +0800

    update

--- 01基础/day01-Java入门/ValueDemo1.java
+++ 01基础/day01-Java入门/ValueDemo1.java
@@ -1,38 +1,51 @@
 public class ValueDemo1 {
-      public static void main(String[] args) {
-            // 字符串类型
-            String str = "Hello World";
-            System.out.println(str);
+    public static void main(String[] args) {
+        // 字符串类型
+        String str = "Hello World";
+        System.out.println(str);
 
-            // 字符类型
-            char ch = 'a';
-            System.out.println(ch);
+        // 字符类型
+        char ch = 'a';
+        System.out.println(ch);
 
-            // 整数类型
-            byte b = 10; // 1个字节
-            short s = 20; // 2个字节
-            int i = 30; // 4个字节
-            long l = 40L;  // 8个字节
-            System.out.println(b);
-            System.out.println(s);
-            System.out.println(i);
-            System.out.println(l);
+        // 整数类型
+        byte b = 10; // 1个字节
+        short s = 20; // 2个字节
+        int i = 30; // 4个字节
+        long l = 40L;  // 8个字节
+        System.out.println(b);
+        System.out.println(s);
+        System.out.println(i);
+        System.out.println(l);
 
-            // 浮点类型
-            float f = 3.14F;
-            double d = 3.14;
-            System.out.println(f);
-            System.out.println(d);
+        // 浮点类型
+        float f = 3.14F;
+        double d = 3.14;
+        System.out.println(f);
+        System.out.println(d);
 
-            // 布尔类型
-            boolean flag = true;
-            System.out.println(flag);
+        // float
+        // 32位（4字节）存储
+        // 32位（4字节）存储。
+        // 有效数字大约为7位小数。
+        // 适用于内存有限且对精度要求不高的场景。
 
-            // 空类型
-            // null 是一个关键字, 代表空, 只能赋值给引用类型
-             String nullStr = null;
-            System.out.println(nullStr);
 
-        }
+        // double
+        // 64位（8字节）存储
+        // 有效数字大约为15位小数。
+        // Java 默认将带小数点的数值视为 double 类型，所以定义 double 类型的常量时，不需要加任何后缀。
+        // 适用于对精度要求较高的场景。
+
+        // 布尔类型
+        boolean flag = true;
+        System.out.println(flag);
+
+        // 空类型
+        // null 是一个关键字, 代表空, 只能赋值给引用类型
+        String nullStr = null;
+        System.out.println(nullStr);
+
+    }
 
 }
--- 01基础/day01-Java入门/ValueDemo2.java
+++ 01基础/day01-Java入门/ValueDemo2.java
@@ -2,7 +2,9 @@ public class ValueDemo2 {
     public static void main(String[] args) {
         // \t 制表符, 在打印的时候, 把前面的字符串长度补齐到8的倍数, 最少补一个空格, 最多补8个空格
         System.out.println("name" + "\t" + "Tom");
+        System.out.println("namename" + "\t" + "Tom");
         System.out.println("age" + "\t" + "23");
         // \n 换行符, 在打印的时候, 把前面的字符串打印完毕, 然后换行
+        System.out.println("gender" + "\t" + "Male" + "\n" + "height" + "\t" + "175");
     }
 }
