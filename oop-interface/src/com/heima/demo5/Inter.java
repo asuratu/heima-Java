b7bf821c54bcff3bb950e4890d2d3771feb48c28
AsuraTu <tugecn@gmail.com>
2024-09-14 10:22:24 +0800

    update

--- oop-interface/src/com/heima/demo5/Inter.java
+++ oop-interface/src/com/heima/demo5/Inter.java
@@ -9,5 +9,27 @@ public interface Inter {
         System.out.println("Inter static show");
     }
 
+    // 静态方法可以使用私有静态方法
+    static void staticMethod() {
+        System.out.println("Static method");
+        staticHelper();  // 调用私有静态方法
+    }
+
+    // 定义一个静态私有方法
+    private static void staticHelper() {
+        System.out.println("Private static method to assist static method");
+    }
+
     void method();
+
+    // 默认方法可以使用私有方法
+    default void defaultMethod() {
+        System.out.println("Default method");
+        commonHelper();  // 调用私有方法
+    }
+
+    // 定义一个普通私有方法
+    private void commonHelper() {
+        System.out.println("Private method to assist default method");
+    }
 }
